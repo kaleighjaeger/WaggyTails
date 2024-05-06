ddd2b2e908afb38686573dd418dfa2ba507dac54
Kaleigh Jaeger <kaleighjaeger@Kaleighs-MacBook-Pro.local>
2024-05-06 13:43:19 -0400

    Initial commit for Spring Boot backend

---
+++ waggytails/src/main/java/com/example/demo/WaggytailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaggytailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaggytailsApplication.class, args);
+	}
+
+}
